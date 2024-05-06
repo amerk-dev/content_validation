8dcfb2a20b34ab1bbb08782c85940eacffa5dd76
p2p-chess <devamerkh@gmail.com>
2024-05-06 15:01:58 +0500

    init

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-"""Module providing an API for NSFW image detection."""
-
 import io
 import hashlib
 import logging
@@ -18,13 +16,10 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-# Initialize Cache with no TTL
 cache = Cache(maxsize=1000)
 
-# Load the model using the transformers pipeline
 model = pipeline("image-classification", model="falconsai/nsfw_image_detection")
 
-# Detect the device used by TensorFlow
 DEVICE = "GPU" if tf.config.list_physical_devices('GPU') else "CPU"
 logging.info("TensorFlow version: %s", tf.__version__)
 logging.info("Model is using: %s", DEVICE)
@@ -33,43 +28,36 @@ if DEVICE == "GPU":
     logging.info("GPUs available: %d", len(tf.config.list_physical_devices("GPU")))
 
 def hash_data(data):
-    """Function for hashing image data."""
     return hashlib.sha256(data).hexdigest()
 
 
-@app.post("/v1/detect")
+@app.post("/detect")
 async def classify_image(file: UploadFile = File(...)):
-    """Function analyzing image."""
     try:
         logging.info("Processing %s", file.filename)
-        # Read the image file
         image_data = await file.read()
         image_hash = hash_data(image_data)
 
         if image_hash in cache:
-            # Return cached entry
             logging.info("Returning cached entry for %s", file.filename)
             return JSONResponse(status_code=200, content=cache[image_hash])
 
         image = Image.open(io.BytesIO(image_data))
 
-        # Use the model to classify the image
         results = model(image)
 
-        # Find the prediction with the highest confidence using the max() function
+
         best_prediction = max(results, key=lambda x: x["score"])
 
-        # Calculate the confidence score, rounded to the nearest tenth and as a percentage
         confidence_percentage = round(best_prediction["score"] * 100, 1)
 
-        # Prepare the custom response data
+
         response_data = {
             "file_name": file.filename,
             "is_nsfw": best_prediction["label"] == "nsfw",
             "confidence_percentage": confidence_percentage,
         }
 
-        # Populate hash
         cache[image_hash] = response_data
 
         return JSONResponse(status_code=200, content=response_data)
